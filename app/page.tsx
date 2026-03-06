7a6d55caa8ff2cc3fe99ad297c55ba1471300de3
Muhammad Farid Islamudin <faridislamudin340@gmail.com>
2026-03-06 09:02:53 +0700

    feat: create 1 dummy login account

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 "use client"
 
 import { useRouter } from "next/navigation"
+import bcrypt from 'bcryptjs';
 import { useEffect } from "react"
 
 export default function page() {
     useEffect(() => {
-         window.location.href = "/landing-page"
+        const payload = {
+            id: "U-" + String(Date.now()).slice(-9),
+            username: "admin",
+            email: "admin@gmail.com",
+            password: bcrypt.hashSync("password"),
+            createdAt: new Date().toISOString(),
+            updatedAt: new Date().toISOString()
+        }
+        localStorage.setItem("users", JSON.stringify(payload))
+        window.location.href = "/landing-page"
     }, [])
 }
